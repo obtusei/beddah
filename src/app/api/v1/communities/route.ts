d2ed6d90dadfb188cd04909b30f828e31b076558
obtusei <54235268+obtusei@users.noreply.github.com>
2023-05-30 19:15:26 +0545

    new changes

--- src/app/api/v1/communities/route.ts
+++ src/app/api/v1/communities/route.ts
@@ -17,4 +17,3 @@ export async function GET(request: Request) {
   },  
   });
 }
-
--- src/app/api/v1/dogs/route.ts
+++ src/app/api/v1/dogs/route.ts
@@ -22,24 +22,24 @@ export async function GET(request: NextRequest) {
   });
 }
 
-export async function POST(request: NextRequest) {
-  try{
+// export async function POST(request: NextRequest) {
+//   try{
 
-    const {name,bio,age,breed,gender,image}:any = await request.json();
-    if (name === undefined || name == "") return error()
-    const newDog = await prisma.pet.create({
-        data:{
-          name:name,
-          bio:bio,
-          age: age,
-          breed: breed,
-          gender: gender,
-          image: image,
-        }
-      })
-      return success(newDog,"Dog created successfully")
-    }
-    catch(e){
-      return error
-    }
-  }
+//     const {name,bio,age,breed,gender,image}:any = await request.json();
+//     if (name === undefined || name == "") return error()
+//     const newDog = await prisma.pet.create({
+//         data:{
+//           name:name,
+//           bio:bio,
+//           age: age,
+//           breed: breed,
+//           gender: gender,
+//           image: image,
+//         }
+//       })
+//       return success(newDog,"Dog created successfully")
+//     }
+//     catch(e){
+//       return error
+//     }
+//   }

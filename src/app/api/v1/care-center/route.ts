8c7f2b0a1090940ab8471eaa02f275cfa145b318
obtusei <54235268+obtusei@users.noreply.github.com>
2023-06-03 18:39:52 +0545

    new changes

--- src/app/api/v1/care-center/route.ts
+++ src/app/api/v1/care-center/route.ts
@@ -3,9 +3,9 @@ import prisma from '@db/prisma';
 import type { NextRequest} from "next/server";
 import formidable from "formidable"
 import { error, success } from 'utils/responses';
-import path from 'path';
-import fs from "fs"
-import { IncomingMessage } from 'http';
+// import path from 'path';
+// import fs from "fs"
+// import { IncomingMessage } from 'http';
 export async function GET(request: NextRequest) {
 
   try{
@@ -19,41 +19,36 @@ export async function GET(request: NextRequest) {
 }
 
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
 
-const readFile = (
-  req: IncomingMessage,
-  saveLocally?: boolean
-): Promise<{ fields: formidable.Fields; files: formidable.Files }> => {
-  const options: formidable.Options = {};
-  if (saveLocally) {
-    options.uploadDir = path.join(process.cwd(), "/public/uploads");
-    options.filename = (name, ext, path, form) => {
-      return Date.now().toString() + "_" + path.originalFilename;
-    };
-  }
-  options.maxFileSize = 4000 * 1024 * 1024;
-  const form = formidable(options);
-  return new Promise((resolve, reject) => {
-    form.parse(req, (err, fields, files) => {
-      if (err) reject(err);
-      resolve({ fields, files });
-    });
-  });
-};
+// const readFile = (
+//   req: IncomingMessage,
+//   saveLocally?: boolean
+// ): Promise<{ fields: formidable.Fields; files: formidable.Files }> => {
+//   const options: formidable.Options = {};
+//   if (saveLocally) {
+//     options.uploadDir = path.join(process.cwd(), "/public/uploads");
+//     options.filename = (name, ext, path, form) => {
+//       return Date.now().toString() + "_" + path.originalFilename;
+//     };
+//   }
+//   options.maxFileSize = 4000 * 1024 * 1024;
+//   const form = formidable(options);
+//   return new Promise((resolve, reject) => {
+//     form.parse(req, (err, fields, files) => {
+//       if (err) reject(err);
+//       resolve({ fields, files });
+//     });
+//   });
+// };
 
-export async function POST(request:IncomingMessage) {
-  try {
-    await fs.readFileSync(path.join(process.cwd() + "/public", "/uploads"));
-  } catch (error) {
-    await fs.mkdirSync(path.join(process.cwd() + "/public", "/uploads"));
-  }
-  await readFile(request, true);
-  return NextResponse.json({
-    message:"Uploaded Successfully"
-  })
-}
+// export async function POST(request:IncomingMessage) {
+//   try {
+//     await fs.readFileSync(path.join(process.cwd() + "/public", "/uploads"));
+//   } catch (error) {
+//     await fs.mkdirSync(path.join(process.cwd() + "/public", "/uploads"));
+//   }
+//   await readFile(request, true);
+//   return NextResponse.json({
+//     message:"Uploaded Successfully"
+//   })
+// }

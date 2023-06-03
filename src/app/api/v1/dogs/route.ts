0f4ae0fce75a98041bb36eb3990108c84ecb04cb
obtusei <54235268+obtusei@users.noreply.github.com>
2023-06-03 11:22:18 +0545

    added logins

--- src/app/api/v1/dogs/route.ts
+++ src/app/api/v1/dogs/route.ts
@@ -22,24 +22,41 @@ export async function GET(request: NextRequest) {
   });
 }
 
-// export async function POST(request: NextRequest) {
-//   try{
+export async function POST(request: NextRequest) {
+  try{
 
-//     const {name,bio,age,breed,gender,image}:any = await request.json();
-//     if (name === undefined || name == "") return error()
-//     const newDog = await prisma.pet.create({
-//         data:{
-//           name:name,
-//           bio:bio,
-//           age: age,
-//           breed: breed,
-//           gender: gender,
-//           image: image,
-//         }
-//       })
-//       return success(newDog,"Dog created successfully")
-//     }
-//     catch(e){
-//       return error
-//     }
-//   }
+    const {name,bio,age,breed,gender,image}:any = await request.json();
+    if (name === undefined || name == "") return error()
+    const newDog = await prisma.pet.create({
+        data:{
+          name:name,
+          bio:bio,
+          age: age,
+          breed: breed,
+          gender: gender,
+          image: image,
+        }
+      })
+      return success(newDog,"Dog created successfully")
+    }
+    catch(e){
+      return error
+    }
+}
+
+export async function DELETE(request: NextRequest) {
+  try{
+
+    const {name,bio,age,breed,gender,image}:any = await request.json();
+    if (name === undefined || name == "") return error()
+    const newDog = await prisma.pet.delete({
+        where:{
+          id:"12323"
+        }
+      })
+      return success(newDog,"Dog created successfully")
+    }
+    catch(e){
+      return error
+    }
+}

fc70fccccb101d9ed99df5ac35c4b503a1f3b2a3
obtusei <54235268+obtusei@users.noreply.github.com>
2023-06-13 10:22:28 +0545

    update

--- src/app/api/v1/communities/route.ts
+++ src/app/api/v1/communities/route.ts
@@ -3,6 +3,13 @@ import { error, success } from 'utils/responses';
 
 export async function GET(request: NextRequest) {
   try{
+    const communityId = request.nextUrl.searchParams.get("id")
+    if (communityId != null){
+      return success({
+        com:"community",
+        id:communityId
+      })
+    } 
     return success({
       com:"communities"
     })

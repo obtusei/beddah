36c0b49425ec13fe158a7e485627ebe75a59e8b9
obtusei <54235268+obtusei@users.noreply.github.com>
2023-06-13 10:16:07 +0545

    update

--- src/app/api/v1/communities/route.ts
+++ src/app/api/v1/communities/route.ts
@@ -5,18 +5,22 @@ import { error, success } from 'utils/responses';
 export async function GET(request: NextRequest) {
   try{
     const id = request.nextUrl.searchParams.get("id")
-    const apple="adas"
-    if (id != null){
-      const community =  await prisma.community.findUnique({
-        where:{
-          id:id
-        }
-      })
-      return success(community);
-    }else{
-    const communities = await prisma.community.findMany();
-    return success(communities)
-    }
+    
+    // if (id != null){
+    //   const community =  await prisma.community.findUnique({
+    //     where:{
+    //       id:id
+    //     }
+    //   })
+    //   return success(community);
+    // }else{
+    // const communities = await prisma.community.findMany();
+    // return success(communities)
+    // }
+    return success({
+      id:id ? id:"123213",
+      com:"communities"
+    })
   }
   catch{
     return error()

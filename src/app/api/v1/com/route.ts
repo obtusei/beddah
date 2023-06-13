e6106e4da22d7cd2232ea77703e9038cdc3b4feb
obtusei <54235268+obtusei@users.noreply.github.com>
2023-06-13 10:51:21 +0545

    update

--- src/app/api/v1/com/route.ts
+++ src/app/api/v1/com/route.ts
@@ -1,20 +1,27 @@
-import type { NextRequest } from 'next/server'
+import { NextResponse, type NextRequest } from 'next/server'
 import prisma from '@db/prisma';
 import { error, success } from 'utils/responses';
 
 export async function GET(request: NextRequest) {
   try{
-    const id = request.nextUrl.searchParams.get("id")
-    if (id != null){
-      const dog =  await prisma.pet.findUnique({
-        where:{
-          id:id
-        }
-      })
-      return success(dog)
+    if (request.nextUrl){
+      const id = request.nextUrl.searchParams.get("id")
+      if (id != null){
+        const dog =  await prisma.pet.findUnique({
+          where:{
+            id:id
+          }
+        })
+        return success(dog)
+      }else{
+        const dogs =  await prisma.pet.findMany()
+        return success(dogs)
+      }
     }else{
-      const dogs =  await prisma.pet.findMany()
-      return success(dogs)
+      return NextResponse.json({
+        status: 400,
+        body: "Bad Request"
+      })
     }
   }catch(er){
     return error()
